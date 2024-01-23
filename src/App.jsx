ea0b82efc335421b8255c717976e9d008f21137f
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-01-23 14:45:33 +0900

    add css and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div>Travel list</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳 </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
